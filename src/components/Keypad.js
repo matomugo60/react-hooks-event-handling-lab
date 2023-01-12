9a0909e47aaae2a76b6db48a37b145fb30ad5386
matomugo60 <matomugo60@gmail.com>
2023-01-12 12:25:44 +0300

    Complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    function handlePasswordChange () {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        // <div></div>
+        <input type="password" onChange={handlePasswordChange}/>
     )
 }
 
